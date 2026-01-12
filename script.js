976305be0a6f15ad2a9d3698bcb33814b784bf22
Ashish Apotikar <ashishapotikar9@gmail.com>
2026-01-12 18:53:30 +0530

    Changed Contact Section

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ setTimeout(type,150);
 }
 
 type();
+
+const contactBtn = document.querySelector(".contact-btn");
+const contactBox = document.querySelector(".contact-popup");
+
+contactBtn.addEventListener("click", function(e){
+  e.preventDefault();
+  if(contactBox.style.display === "block"){
+    contactBox.style.display = "none";
+  } else {
+    contactBox.style.display = "block";
+  }
+});
